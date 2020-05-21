b56d0f6e576692bad0186b7a7cb22bf60bc7486f
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-21 16:58:10 -0300

    se creo Interface Clientes en clientes component. También un Array<Clientes>, se guardo los datos en el navegador y los mismo fueron comentados

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Clientes{
+  nombre: string,
+  apellido: string,
+  edad: number
+}
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -7,9 +13,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  clientes: Array<Clientes> = new Array<Clientes>();
   constructor() { }
 
   ngOnInit(): void {
+    // this.clientes.push({
+    //   nombre: "Julián",
+    //   apellido: "Pérez Pesce",
+    //   edad: 35
+    // },
+    // {
+    //   nombre: "Maria José",
+    //   apellido: "Gonzales",
+    //   edad: 26
+    // })
   }
 
+  guardarCliente(){
+    localStorage.setItem("cliente", JSON.stringify(this.clientes));
+  }
 }
