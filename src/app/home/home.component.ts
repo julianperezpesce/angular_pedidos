534d35853fe5be43e73da40c7fa69caa13221055
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-22 20:09:40 -0300

    Inyectamos ClientesService en home.component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../services/clientes.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public clientesServicio: ClientesService) { }
 
   ngOnInit(): void {
   }
