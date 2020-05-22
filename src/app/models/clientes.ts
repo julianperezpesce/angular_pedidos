08da80b38c24b934b08f39b5d0caf5e0e6d7f098
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-22 16:50:47 -0300

    se creo la clase models/clientes.ts, y se establecion los parametros de la misma y su constructor para instanciar

---
+++ src/app/models/clientes.ts
@@ -0,0 +1,14 @@
+export class Clientes{
+    nombre: string;
+    apellido: string;
+    direccion: string;
+    clienteId: number;
+
+
+    constructor (){
+        this.nombre = this.nombre;
+        this.apellido = this.apellido;
+        this.direccion = this.direccion;
+        this.clienteId = this.clienteId;        
+    }
+}
