754bda0aedfd29b247a6ba53a4f70b98d384a893
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-22 20:20:37 -0300

    se creo clientes:Array<Clientes>. tb se lo agrego home cop html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ClientesService } from '../services/clientes.service';
+import { Clientes } from '../models/clientes';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,11 @@ import { ClientesService } from '../services/clientes.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  clientes: Array<Clientes> = new Array<Clientes>();
   constructor(public clientesServicio: ClientesService) { }
 
   ngOnInit(): void {
+    this.clientes = this.clientesServicio.clientesLocalStorage;
   }
 
 }
