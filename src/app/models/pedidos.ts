96299c282e1079765d6be8e9e4f114b4b2996322
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-23 18:51:16 -0300

    se agrega parametro pedidoId a pedidos.ts

--- src/app/models/pedidos.ts
+++ src/app/models/pedidos.ts
@@ -1,12 +1,14 @@
 import { PedidoDetalle } from './pedidoDetalle';
 
 export class Pedidos{
+    pedidoId: number;
     clienteId: number;
     nombreCliente: string;
     totalDelPedido: number;
     pedidoDetalle: Array<PedidoDetalle>;
 
     constructor(){
+        this.pedidoId = this.pedidoId;
         this.clienteId = this.clienteId;
         this.nombreCliente = this.nombreCliente;
         this.totalDelPedido = this.totalDelPedido;
