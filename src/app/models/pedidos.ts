c91cbf65b0fcbaa65869694ed4191d1d3089a8b5
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-23 18:36:10 -0300

    clase pedido

---
+++ src/app/models/pedidos.ts
@@ -0,0 +1,11 @@
+export class Pedidos{
+    clienteId: number;
+    nombreCliente: string;
+    totalDelPedido: number;
+
+    constructor(){
+        this.clienteId = this.clienteId;
+        this.nombreCliente = this.nombreCliente;
+        this.totalDelPedido = this.totalDelPedido;
+    }
+}
