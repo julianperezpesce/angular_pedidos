9515356924e91fdcbf07d0074c077517548f493e
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-23 19:35:23 -0300

    metodo agregarProducto en pedidos.component.ts

--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Pedidos } from '../models/pedidos';
+import { Productos } from '../models/productos';
+import { PedidoDetalle } from '../models/pedidoDetalle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PedidosService {
   pedido: Pedidos = new Pedidos();
-  
+
   constructor() { }
 
+  agregarProducto(producto: Productos){
+    let pedidoDetalle: PedidoDetalle = new PedidoDetalle();
+    pedidoDetalle.cantidad = 1;
+    pedidoDetalle.nombre = producto.nombre;
+    pedidoDetalle.precio = producto.precio;
+    pedidoDetalle.productoId = producto.productoId;
+    pedidoDetalle.total = pedidoDetalle.cantidad * pedidoDetalle.precio;
+    this.pedido.pedidoDetalle.push(pedidoDetalle);
+  }
 
 }
