2115d9b80801949920a78089fff3fb2f0d2516c0
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-23 18:39:32 -0300

    creacion de clase PedidoDetalle

---
+++ src/app/models/pedidoDetalle.ts
@@ -0,0 +1,15 @@
+export class PedidoDetalle{
+    productoId: number;
+    nombre: string;
+    cantidad: number;
+    precio: number;
+    total: number;
+
+    constructor(){
+        this.productoId = this.productoId;
+        this.nombre = this.nombre;
+        this.cantidad = this.cantidad;
+        this.precio = this.precio;
+        this.total = this.total;
+    }
+}
