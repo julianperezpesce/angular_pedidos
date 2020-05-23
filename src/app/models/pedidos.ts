83248fd77ddbbec82ec6a120dbb2577ce0a28c24
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-23 18:44:04 -0300

    se creo un Array<PedidoDetalle> en Pedidos.ts

--- src/app/models/pedidos.ts
+++ src/app/models/pedidos.ts
@@ -1,11 +1,15 @@
+import { PedidoDetalle } from './pedidoDetalle';
+
 export class Pedidos{
     clienteId: number;
     nombreCliente: string;
     totalDelPedido: number;
+    pedidoDetalle: Array<PedidoDetalle>;
 
     constructor(){
         this.clienteId = this.clienteId;
         this.nombreCliente = this.nombreCliente;
         this.totalDelPedido = this.totalDelPedido;
+        this.pedidoDetalle = new Array<PedidoDetalle>();
     }
 }
