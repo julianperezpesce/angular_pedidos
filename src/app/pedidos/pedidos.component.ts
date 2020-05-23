d0a00963f4119028c53dfbd29c96e29301b3e695
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-23 19:37:31 -0300

    se traslado el metodo agregarProducto a pedidos.component.ts

--- src/app/pedidos/pedidos.component.ts
+++ src/app/pedidos/pedidos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Pedidos } from '../models/pedidos';
 import { PedidosService } from '../services/pedidos.service';
+import { Productos } from '../models/productos';
+import { PedidoDetalle } from '../models/pedidoDetalle';
 
 @Component({
   selector: 'app-pedidos',
@@ -9,10 +11,21 @@ import { PedidosService } from '../services/pedidos.service';
 })
 export class PedidosComponent implements OnInit {
 
+  pedido: Pedidos = new Pedidos();
   constructor(public pedidosService: PedidosService) { }
 
   ngOnInit(): void {
     
+  }
+
+  agregarProducto(producto: Productos){
+    let pedidoDetalle: PedidoDetalle = new PedidoDetalle();
+    pedidoDetalle.cantidad = 1;
+    pedidoDetalle.nombre = producto.nombre;
+    pedidoDetalle.precio = producto.precio;
+    pedidoDetalle.productoId = producto.productoId;
+    pedidoDetalle.total = pedidoDetalle.cantidad * pedidoDetalle.precio;
+    this.pedido.pedidoDetalle.push(pedidoDetalle);
   }
 
 }
--- src/app/services/pedidos.service.ts
+++ src/app/services/pedidos.service.ts
@@ -7,18 +7,10 @@ import { PedidoDetalle } from '../models/pedidoDetalle';
   providedIn: 'root'
 })
 export class PedidosService {
-  pedido: Pedidos = new Pedidos();
+  
 
   constructor() { }
 
-  agregarProducto(producto: Productos){
-    let pedidoDetalle: PedidoDetalle = new PedidoDetalle();
-    pedidoDetalle.cantidad = 1;
-    pedidoDetalle.nombre = producto.nombre;
-    pedidoDetalle.precio = producto.precio;
-    pedidoDetalle.productoId = producto.productoId;
-    pedidoDetalle.total = pedidoDetalle.cantidad * pedidoDetalle.precio;
-    this.pedido.pedidoDetalle.push(pedidoDetalle);
-  }
+  
 
 }
