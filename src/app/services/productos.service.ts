1b7ac9d03b4754976a981a609df15c845150afb7
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-22 21:52:43 -0300

    edicion del servicio productos

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Productos } from '../models/productos';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,20 @@ import { Injectable } from '@angular/core';
 export class ProductosService {
 
   constructor() { }
+
+  agregarProductoLocalStorage(producto: Productos){
+    let productosAnteriores: Productos[] = this.productosLocalStorage;
+    producto.productoId = productosAnteriores.length +1;
+    productosAnteriores.push(producto);
+
+    localStorage.setItem('productos', JSON.stringify(productosAnteriores))
+  }
+
+  get productosLocalStorage(): Productos[]{
+    let obtenerProductosDeLocalStorage: Productos[] = JSON.parse(localStorage.getItem('productos'));
+    if (obtenerProductosDeLocalStorage == null) {
+      return new Array<Productos>();
+    }
+    return obtenerProductosDeLocalStorage;
+  }
 }
