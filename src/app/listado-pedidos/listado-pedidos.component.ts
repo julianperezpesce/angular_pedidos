64e4cf7cdd301f25def01c5cc1fd02822c6229ee
Julián Pérez Pesce <julianperezpesce@users.noreply.github.com>
2020-05-24 22:11:52 -0300

    Inyeccion de PedidosService  listado-Pedidos.co.ts

--- src/app/listado-pedidos/listado-pedidos.component.ts
+++ src/app/listado-pedidos/listado-pedidos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PedidosService } from '../services/pedidos.service';
 
 @Component({
   selector: 'app-listado-pedidos',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoPedidosComponent implements OnInit {
 
-  constructor() { }
+  constructor(public pedidosService: PedidosService) { }
 
   ngOnInit(): void {
   }
